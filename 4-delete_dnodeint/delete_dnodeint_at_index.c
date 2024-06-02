59e6d49846944d6624ae0a48823c3b657899723e
tech-gecko <w0nderzz2001@gmail.com>
2024-06-01 21:06:53 -0700

    Fix all codes

--- 4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -43,10 +43,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		(*head)->prev->prev = (*head)->prev;
-		free(*head);
+		if ((*head)->prev)
+			(*head)->prev->next = (*head)->next;
 		if ((*head)->next)
 			(*head)->next->prev = (*head)->prev;
+		free(*head);
 		*head = saved_head;
 	}
 	return (1);
